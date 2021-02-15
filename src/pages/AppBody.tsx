9da11c1a3cc6e1df20b2aa0d6a18eab994677b01
Geoff <geoff@fiteanalytics.com>
2021-02-15 13:44:12 -0800

    geoff

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -16,5 +16,5 @@ export const BodyWrapper = styled.div`
  * The styled container element that wraps the content of most pages and the tabs.
  */
 export default function AppBody({ children }: { children: React.ReactNode }) {
-  return <BodyWrapper>{children}</BodyWrapper>
+  return <BodyWrapper>{children} ***<h3>THIS APP IS IN ALPHA MODE DO NOT USE</h3>*** </BodyWrapper>
 }
